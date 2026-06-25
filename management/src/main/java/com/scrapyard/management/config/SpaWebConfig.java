ca954665d56df12caa3c1c05c515b059b8178bfd
superadminsyms-ui <superadminsyms@gmail.com>
2026-06-24 21:36:11 -0500

    Update SpaWebConfig.java

--- management/src/main/java/com/scrapyard/management/config/SpaWebConfig.java
+++ management/src/main/java/com/scrapyard/management/config/SpaWebConfig.java
@@ -1,16 +1,19 @@
-package com.scrapyard.management.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpaWebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        ViewControllerRegistration registration = registry.addViewController("/**");
-        registration.setViewName("forward:/index.html");
-    }
-}
+ package com.scrapyard.management.config;                                                                                                                          
+                                                                                                                                                                                    
+     import org.springframework.context.annotation.Configuration;                                                                                                              
+     import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;                                                                             
+     import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;                                                                                                     
+                                                                                                                                                                                    
+     @Configuration                                                                                                                                                                 
+     public class SpaWebConfig implements WebMvcConfigurer {                                                                                                                        
+                                                                                                                                                                                    
+         @Override                                                                                                                                                                  
+         public void addViewControllers(ViewControllerRegistry registry) {                                                                                                          
+             registry.addViewController("/login").setViewName("forward:/index.html");                                                                                               
+             registry.addViewController("/forgot-password").setViewName("forward:/index.html");                                                                                     
+             registry.addViewController("/reset-password").setViewName("forward:/index.html");                                                                                      
+             registry.addViewController("/change-password").setViewName("forward:/index.html");                                                                                     
+             registry.addViewController("/app").setViewName("forward:/index.html");                                                                                                 
+             registry.addViewController("/app/**").setViewName("forward:/index.html");                                                                                              
+         }                                                                                                                                                                          
+     }  
